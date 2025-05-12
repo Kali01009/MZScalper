8c3b7854471d73b1fdc5f44f2ecab8d1f4cdf5b2
khauta seabata <75206578+Kali01009@users.noreply.github.com>
2025-05-12 01:34:32 -0800

    notifier.py

--- notifier.py
+++ notifier.py
@@ -4,7 +4,6 @@ import os
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
 TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
 
-
 def send_telegram_message(message: str):
     """Send formatted alert to Telegram"""
     url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
